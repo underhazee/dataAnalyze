29029db80cb8a0d43b43414f684fb9272b49c2f2
VoinovVladyslav <silveraquaris771002@gmail.com>
2022-04-03 20:40:02 +0300

    working in right way

--- parser.py
+++ parser.py
@@ -5,3 +5,16 @@ url = 'https://www.scimagojr.com/journalrank.php?page=1&total_size=32958'
 page = requests.get(url)
 
 print(page.status_code)
+
+soup = BeautifulSoup(page.text, "html.parser")
+all = []
+
+tag = soup.tbody
+
+all = tag.findAll('tr')
+
+for i in all:
+    alltd = i.findAll('td')
+    for j in alltd:
+        print(j)
+    print("\n")
